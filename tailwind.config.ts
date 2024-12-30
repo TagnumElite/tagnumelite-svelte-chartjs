88ebe3c95cc758685c659e09840b165929445ed9
Tagan H <tagnum.elite@gmail.com>
2024-12-30 16:20:20 +0200

    Format tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,7 @@ import type { Config } from 'tailwindcss';
 import FlowbitePlugin from 'flowbite/plugin';
 
 export default {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './node_modules/flowbite/**/*.js'
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite/**/*.js'],
 
   theme: {
     extend: {}
